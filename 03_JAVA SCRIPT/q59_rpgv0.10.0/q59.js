ff56cbf5ada4eca80d5ecd27517b4a5e71e087f3
seungcheol <95950466+seuungcheol@users.noreply.github.com>
2021-12-23 18:21:53 +0900

    Add files via upload

---
+++ 03_JAVA SCRIPT/q59_rpgv0.10.0/q59.js
@@ -0,0 +1,72 @@
+//몬스터 및 캐릭터 정보//
+function displayCharacterInfo(){
+	if (orc.hp < elf.hp){
+		elf.exp = elf.exp + orc.exp;
+	}
+	orc.info();
+	elf.info();
+}
+
+
+function battleTurn(){
+	var comDamage = getRandomAttackValue(orc.attack); //몬스터 공격//
+	var userDamage = getRandomAttackValue(elf.attack);//캐릭터 공격//
+	
+	orc.hp = orc.hp - userDamage;
+	dw(elf.name + "가 " + orc.name + "에게 데미지를 "+userDamage +" 입혔습니다.\n\n");
+	elf.hp = elf.hp - comDamage;
+	dw(orc.name + "가 " + elf.name + "에게 데미지를 "+comDamage+" 입혔습니다.\n\n");
+	if (orc.hp <= 0 || elf.hp <= 0){
+		return false;
+	} else {
+		return true;
+	}
+}
+
+//전투 결과
+function result(){
+	dw("전투 종료!!\n\n");
+	if (orc.hp < elf.hp){
+		dw(elf.name+"가"+orc.name+"를 처치 하였습니다.\n");
+		dw(orc.name+"에게"+orc.exp+"ex와"+orc.gold+"골드를 얻었습니다.\n\n");
+	} else {
+		dw(elf.name+ "가 사망 하였습니다.\n\n");
+	}
+	
+}
+
+
+//몬스터 및 캐릭터 random 공격력 계산//
+function getRandomAttackValue(attack){						
+	attack = attack + 1;    //ex. 공격력이 10이라고 하면 0~10의 수치로 나와야 하므로					
+	var random = Math.floor(Math.random()*attack);  // ex. 공격력이 11이고 이 랜덤처리를 하면 랜덤 값은 0~10이 나옴					
+	return random;					
+}
+
+
+
+
+var screen;
+var screenPlaye;
+var screenCom;
+var orc = new Monster("오크", 2500, 150, 5000, 100);
+var elf = new Character("엘프", 2000, 200, 15000, 0);
+
+
+window.onload = function(){
+	screen = document.getElementById("screen");
+	screenPlayer = document.getElementById("screenPlayer");
+	screenCom = document.getElementById("screenCom");
+	
+	displayCharacterInfo();//info//
+	
+	dw("전투 시작!!\n\n");
+	
+	var rotation = true
+	while (rotation){
+		rotation = battleTurn();
+	}
+	result();
+	displayCharacterInfo();//info//
+	
+}
